fc5fb9245c2af904e12c6dc5d4401b79342cd0c3
steven francisco & bob ellis <student@epicodus.com>
2015-05-13 16:48:14 -0700

    Add new front end pages

--- app.rb
+++ app.rb
@@ -19,8 +19,8 @@ end
 get('/recipe/:id') do
   @id = params.fetch('id')
   @recipe = Recipe.find(@id)
-  @ingredients = Ingredient.all()
-  erb(:recipe_add)
+  @ingredients = @recipe.ingredients()
+  erb(:recipe_info)
 end
 
 get('/add_ingredients/') do
@@ -32,4 +32,12 @@ post('/ingredient_save/') do
   Ingredient.create({:name => params.fetch("ingredient_name")})
   @ingredients = Ingredient.all()
   erb(:ingredients)
+end
+
+delete('/recipe/:id') do
+  @id = params.fetch('id')
+  @recipe = Recipe.find(@id)
+  @recipe.destroy()
+  @recipes = Recipe.all()
+  erb(:index)
 end
